9dbd1900f9314789b13caa99d754886742448b9a
newatcoding <lakshay@attentive.ai>
2024-01-28 23:50:26 +0530

    [init] Striver sheet

---
+++ readMe.md
@@ -0,0 +1,4 @@
+#Striver sheet Solutions
+
+https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/
+https://takeuforward.org/interview-sheets/strivers-79-last-moment-dsa-sheet-ace-interviews/
---
+++ src/Arrays/nextPermutation.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+//Idea is to find a number from last that has just a lower number next to it on both end and finding a
+//place to swap it.
+public class nextPermutation {
+    void nextPermutationSolve(int[] num){
+        int length=nums.length;
+        int swapElement=0;
+        for(int i=length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                swapElement=i;
+                break;
+            }
+        }
+        int swapb=length-1;
+        for(int i=length-1;i>swapElement;i--){
+            if(nums[i]>nums[swapElement]){
+                swapb=i;
+                break;
+            }
+        }
+        int a=nums[swapElement];
+        nums[swapElement]=nums[swapb];
+        nums[swapb]=a;
+
+        //now sort from swapelement +1 to end;
+        Arrays.sort(nums,swapElement+1,length);
+    }
+}
