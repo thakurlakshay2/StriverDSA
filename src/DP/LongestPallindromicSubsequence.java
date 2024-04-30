48be8db56d0f59b0910e9a0aae7421a39543f446
thakurlakshay2 <thakurlakshay2@gmail.com>
2024-04-30 21:32:15 +0530

    [day 33.5] question revision and modification

---
+++ src/DP/LongestPallindromicSubsequence.java
@@ -0,0 +1,56 @@
+package DP;
+
+
+import java.util.Arrays;
+
+//https://takeuforward.org/data-structure/longest-palindromic-subsequence-dp-28/
+public class LongestPallindromicSubsequence {
+
+    //
+    //time : O(n*n)
+    //space : O(n*n)
+    static int lcs(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        // Create a 2D array to store the LCS lengths
+        int dp[][] = new int[n + 1][m + 1];
+
+        // Initialize the dp array with -1
+        for (int rows[] : dp)
+            Arrays.fill(rows, -1);
+
+        // Initialize the first row and first column with 0
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
+        }
+
+        // Fill the dp array using a bottom-up approach
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1))
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                else
+                    dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    // Function to find the length of the Longest Palindromic Subsequence
+    static int longestPalindromeSubsequence(String s) {
+
+        String reversed = new StringBuilder(s).reverse().toString();
+
+
+        return lcs(s, reversed);
+    }
+
+    //One additional question , minimum insertion to make a string palindromic ,
+    // if it not stated you can only add character in front or back.
+    // ans = length - lps();
+}
